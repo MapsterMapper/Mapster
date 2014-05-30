2294de83b36940a78326819ae5af70a8594974b2
eswann <eric@ericswann.org>
2014-05-30 18:05:01 -0500

    Fixed language

--- src/Fpr/Properties/AssemblyInfo.cs
+++ src/Fpr/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fpr")]
-[assembly: AssemblyDescription("A fast, fun and stimulating object to object mappe")]
+[assembly: AssemblyDescription("A fast, fun and stimulating object to object mapper")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fpr")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.*")]
+[assembly: AssemblyVersion("1.1.1.*")]
+[assembly: AssemblyFileVersion("1.1.1.*")]
