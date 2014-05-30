911b164732829a01db07e4724c84a45b440dabad
eswann <eric@ericswann.org>
2014-05-30 17:54:40 -0500

    Added the nuspec

--- src/Fapper/Properties/AssemblyInfo.cs
+++ src/Fapper/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fpr")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A fast, fun and stimulating object to object mappe")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fpr")]
-[assembly: AssemblyCopyright("Copyright © Timucin Kivanc  2014")]
+[assembly: AssemblyCopyright("Copyright © Swannee 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyFileVersion("1.1.0.*")]
