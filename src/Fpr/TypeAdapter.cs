dd12958639e5a33d4748948e277f9e1b0c103e76
eswann <eric@ericswann.org>
2014-07-27 22:04:19 -0500

    Added some comments to support intellisense.

--- src/Fpr/TypeAdapter.cs
+++ src/Fpr/TypeAdapter.cs
@@ -11,31 +11,71 @@ namespace Fpr
         private static readonly Dictionary<int, FastInvokeHandler> _cache = new Dictionary<int, FastInvokeHandler>();
         private static readonly object _cacheLock = new object();
 
+        /// <summary>
+        /// Adapte the source object to the destination type.
+        /// </summary>
+        /// <typeparam name="TDestination">Destination type.</typeparam>
+        /// <param name="source">Source object to adapt.</param>
+        /// <returns>Adapted destination type.</returns>
         public static TDestination Adapt<TDestination>(object source)
         {
             return (TDestination)GetAdapter(source.GetType(), typeof(TDestination))(null, new[] { source });
         }
 
+        /// <summary>
+        /// Adapte the source object to the destination type.
+        /// </summary>
+        /// <typeparam name="TSource">Source type.</typeparam>
+        /// <typeparam name="TDestination">Destination type.</typeparam>
+        /// <param name="source">Source object to adapt.</param>
+        /// <returns>Adapted destination type.</returns>
         public static TDestination Adapt<TSource, TDestination>(TSource source)
         {
             return (TDestination)GetAdapter<TSource, TDestination>()(null, new object[] { source });
         }
 
+        /// <summary>
+        /// Adapte the source object to the existing destination object.
+        /// </summary>
+        /// <typeparam name="TSource">Source type.</typeparam>
+        /// <typeparam name="TDestination">Destination type.</typeparam>
+        /// <param name="source">Source object to adapt.</param>
+        /// <param name="destination">The destination object to populate.</param>
+        /// <returns>Adapted destination type.</returns>
         public static TDestination Adapt<TSource, TDestination>(TSource source, TDestination destination)
         {
             return (TDestination)GetAdapter<TSource, TDestination>(true)(null, new object[] { source, destination });
         }
 
+        /// <summary>
+        /// Adapte the source object to the destination type.
+        /// </summary>
+        /// <param name="source">Source object to adapt.</param>
+        /// <param name="sourceType">The type of the source object.</param>
+        /// <param name="destinationType">The type of the destination object.</param>
+        /// <returns>Adapted destination type.</returns>
         public static object Adapt(object source, Type sourceType, Type destinationType)
         {
             return GetAdapter(sourceType, destinationType)(null, new[] { source });
         }
 
+        /// <summary>
+        /// Adapte the source object to an existing destination object.
+        /// </summary>
+        /// <param name="source">Source object to adapt.</param>
+        /// <param name="destination">Destination object to populate.</param>
+        /// <param name="sourceType">The type of the source object.</param>
+        /// <param name="destinationType">The type of the destination object.</param>
+        /// <returns>Adapted destination type.</returns>
         public static object Adapt(object source, object destination, Type sourceType, Type destinationType)
         {
             return GetAdapter(sourceType, destinationType, true)(null, new[] { source, destination });
         }
 
+        /// <summary>
+        /// Returns an instance representation of the adapter, mainly for DI/IOC situations.
+        /// </summary>
+        /// <returns>Instance of the adapter.</returns>
         public static IAdapter GetInstance()
         {
             return new Adapter();
