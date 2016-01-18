8bbe5a64e6acb27b17f0e4d816a7bacd5ba5b0d8
chaowlert <chaowlert.chaisrichalermpol@allianz.com>
2016-01-18 17:50:29 +0700

    add unit test

---
+++ src/Mapster.Tests/WhenUsingConverterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace Mapster.Tests
+{
+    [TestFixture]
+    public class WhenUsingConverterFactory
+    {
+
+        [Test]
+        public void Custom_Mapping_From_String_To_Char_Array()
+        {
+            TypeAdapterConfig<string, char[]>.NewConfig()
+                .MapWith(str => str.ToCharArray());
+
+            var chars = TypeAdapter.Adapt<char[]>("Hello");
+
+            chars.Length.ShouldEqual(5);
+            chars[0].ShouldEqual('H');
+            chars[1].ShouldEqual('e');
+            chars[2].ShouldEqual('l');
+            chars[3].ShouldEqual('l');
+            chars[4].ShouldEqual('o');
+        }
+    }
+}
---
+++ src/Mapster.Tests/WhenUsingRuleBasedSetting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace Mapster.Tests
+{
+    [TestFixture]
+    public class WhenUsingRuleBasedSetting
+    {
+
+        [Test]
+        public void Rule_Base_Testing()
+        {
+            TypeAdapterConfig.GlobalSettings.When((srcType, destType, mapType) => srcType == destType)
+                .Ignore("Id");
+
+            var simplePoco = new SimplePoco {Id = Guid.NewGuid(), Name = "TestName"};
+
+            var dto = TypeAdapter.Adapt<SimplePoco>(simplePoco);
+
+            dto.Id.ShouldEqual(Guid.Empty);
+            dto.Name.ShouldEqual(simplePoco.Name);
+        }
+
+        #region TestClasses
+
+        public class SimplePoco
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        #endregion
+
+
+    }
+}
