591b8b2880c1a8006ec579aeec7e4f84a1866025
chaowlert <chaowlert.chaisrichalermpol@allianz.com>
2016-02-12 14:23:06 +0700

    Fix ignore null values

---
+++ src/Mapster.Tests/WhenMappingIgnoreNullValues.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Mapster.Tests
+{
+    [TestFixture]
+    public class WhenMappingIgnoreNullValues
+    {
+        [Test]
+        public void Map()
+        {
+            TypeAdapterConfig.GlobalSettings.Clear();
+            TypeAdapterConfig.GlobalSettings.NewConfig<SourceClass, DestClass>()
+                .IgnoreNullValues(true)
+                .Compile();
+
+            var source = new SourceClass();
+            var dest = source.Adapt<DestClass>();
+
+            Assert.AreEqual("Hello", dest.Title);
+            Assert.NotNull(dest.Sub);
+            Assert.NotNull(dest.List);
+        }
+
+        [Test]
+        public void Map_To_Target()
+        {
+            TypeAdapterConfig.GlobalSettings.Clear();
+            TypeAdapterConfig.GlobalSettings.NewConfig<SourceClass, DestClass>()
+                .IgnoreNullValues(true)
+                .Compile();
+
+            var source = new SourceClass();
+            var dest = source.Adapt(new DestClass());
+
+            Assert.AreEqual("Hello", dest.Title);
+            Assert.NotNull(dest.Sub);
+            Assert.NotNull(dest.List);
+        }
+
+        public class DestClass
+        {
+            public string Title { get; set; }
+            public List<string> List { get; set; }
+            public SubClass Sub { get; set; }
+
+            public DestClass()
+            {
+                List = new List<string>();
+                Sub = new SubClass();
+                Title = "Hello";
+            }
+        }
+
+        public class SourceClass
+        {
+            public string Title { get; set; }
+            public List<string> List { get; set; }
+            public SubClass Sub { get; set; }
+        }
+
+        public class SubClass
+        {
+            public string SubName { get; set; }
+        }
+    }
+}
