c8f34f4cd3aa3f87f48221f26c327c31299032a3
chaowlert <chaowlert@gmail.com>
2017-05-15 23:30:23 +0700

    fix net core build

--- src/Mapster/TypeAdapterConfig.cs
+++ src/Mapster/TypeAdapterConfig.cs
@@ -512,7 +512,14 @@ namespace Mapster
 
         private Hashtable _inlineConfigs;
         public TypeAdapterConfig Fork(Action<TypeAdapterConfig> action,
-            [CallerFilePath]string key1 = null, [CallerLineNumber]int key2 = 0)
+#if !NET40
+            [CallerFilePath]
+#endif
+            string key1 = null,
+#if !NET40
+            [CallerLineNumber]
+#endif
+            int key2 = 0)
         {
             if (_inlineConfigs == null)
                 _inlineConfigs = new Hashtable();
