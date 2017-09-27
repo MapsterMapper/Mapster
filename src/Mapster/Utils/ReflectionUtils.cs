82479314797ec296da41b14285cf52ad6782093a
Stano Peťko <stano@stanopetko.eu>
2017-09-27 22:24:50 +0200

    ReflectionUtils.IsPoco does not check for IEnumerable.

--- src/Mapster/Utils/ReflectionUtils.cs
+++ src/Mapster/Utils/ReflectionUtils.cs
@@ -42,10 +42,6 @@ namespace Mapster
 
         public static bool IsPoco(this Type type, BindingFlags accessorFlags = BindingFlags.Public)
         {
-            //not collection
-            if (type.IsCollection())
-                return false;
-
             //not nullable
             if (type.IsNullable())
                 return false;
