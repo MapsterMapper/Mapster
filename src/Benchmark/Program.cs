c0b24f6fb6bc9e0dacec37dfa36b6bafb97d699a
Chaowlert <chaowlert@th.ibm.com>
2018-10-09 14:39:26 +0700

    add FastExpressionCompiler into Benchmark

--- src/Benchmark/Program.cs
+++ src/Benchmark/Program.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using AutoMapper;
 using Benchmark.Classes;
 using Mapster;
+using FastExpressionCompiler;
+using System.Linq.Expressions;
 
 namespace Benchmark
 {
-    internal class Program
+    class Program
     {
-        private static double AutomapperTime;
-        private static double MapsterTime;
-        private static double ExpressMapperTime;
-        private static double TotalAutomapperTime;
-        private static double TotalMapsterTime;
-        private static double TotalExpressMapperTime;
+        static double sAM;
+        static double sMP;
+        static double sMF;
+        static double sEM;
 
+        static Func<LambdaExpression, Delegate> defaultCompiler = TypeAdapterConfig.GlobalSettings.Compiler;
 
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             try
             {
@@ -27,16 +27,21 @@ namespace Benchmark
                     cfg.CreateMap<Foo, Foo>();
                     cfg.CreateMap<Address, Address>();
                     cfg.CreateMap<Address, AddressDTO>();
-                    cfg.CreateMap<Customer, CustomerDTO>();                   
+                    cfg.CreateMap<Customer, CustomerDTO>();
                 });
                 TestSimpleTypes();
                 TestComplexTypes();
 
                 Console.WriteLine();
-                Console.WriteLine("Automapper v7.0.1 to Mapster v3.1.8 ratio: " + (TotalAutomapperTime / TotalMapsterTime).ToString("###.00") + " X slower");
-                Console.WriteLine("ExpressMapper v1.9.1 to Mapster v3.1.8 ratio: " + (TotalExpressMapperTime / TotalMapsterTime).ToString("###.00") + " X slower");
+                Console.WriteLine($"======================================================================================");
+                Console.WriteLine($"| Tests                | Time (ms) | Slower than Mapster | Slower than Mapster + FEC |");
+                Console.WriteLine($"======================================================================================");
+                Console.WriteLine($"| Mapster v3.1.8       | {  sMP,9} | { sMP / sMP,18:N2}X | {       sMP / sMF,24:N2}X |");
+                Console.WriteLine($"| Mapster v3.1.8 + FEC | {  sMF,9} | { sMF / sMP,18:N2}X | {       sMF / sMF,24:N2}X |");
+                Console.WriteLine($"| Automapper v7.0.1    | {  sAM,9} | { sAM / sMP,18:N2}X | {       sAM / sMF,24:N2}X |");
+                Console.WriteLine($"| ExpressMapper v1.9.1 | {  sEM,9} | { sEM / sMP,18:N2}X | {       sEM / sMF,24:N2}X |");
+                Console.WriteLine($"======================================================================================");
                 Console.WriteLine();
-
             }
 
             finally
@@ -46,23 +51,12 @@ namespace Benchmark
             }
         }
 
-        private static void TestSimpleTypes()
+        static void TestSimpleTypes()
         {
-            AutomapperTime = 0;
-            MapsterTime = 0;
-            ExpressMapperTime = 0;
-
             Console.WriteLine("Test 1 : Simple Types");
             Console.WriteLine("Competitors : Mapster, ExpressMapper, AutoMapper");
 
             var foo = GetFoo();
-
-            Mapper.Map<Foo, Foo>(foo);
-            //Mapper.Configuration
-
-            TypeAdapter.Adapt<Foo, Foo>(foo); // cache mapping strategy
-
-            ExpressMapper.Mapper.Map<Foo, Foo>(foo);
 
             TestSimple(foo, 1000);
 
@@ -79,12 +73,8 @@ namespace Benchmark
 
         }
 
-        private static void TestComplexTypes()
+        static void TestComplexTypes()
         {
-            AutomapperTime = 0;
-            MapsterTime = 0;
-            ExpressMapperTime = 0;
-
             Console.WriteLine();
             Console.WriteLine();
 
@@ -93,13 +83,10 @@ namespace Benchmark
 
             var customer = GetCustomer();
 
-            Mapper.Map<Customer, CustomerDTO>(customer);
 
             //TypeAdapterConfig.GlobalSettings.DestinationTransforms.Upsert<Guid>(x => x);
-            TypeAdapter.Adapt<Customer, CustomerDTO>(customer); // cache mapping strategy
 
             //ObjectMapperManager.DefaultInstance.GetMapper<Customer, CustomerDTO>().Map(customer);
-            ExpressMapper.Mapper.Map<Customer, CustomerDTO>(customer);
 
             Test(customer, 1000);
 
@@ -117,7 +104,7 @@ namespace Benchmark
             //Console.WriteLine();
         }
 
-        private static void Test(Customer item, int iterations)
+        static void Test(Customer item, int iterations)
         {
             Console.WriteLine();
 
@@ -125,30 +112,53 @@ namespace Benchmark
 
             //TestCustomerNative(item, iterations);
 
-            TestMapsterAdapter<Customer, CustomerDTO>(item, iterations);
+            TypeAdapterConfig.GlobalSettings.Compiler = defaultCompiler;    //switch compiler
+            TypeAdapterConfig.GlobalSettings.Compile(typeof(Customer), typeof(CustomerDTO));    //recompile
+            item.Adapt<Customer, CustomerDTO>();    //exercise
+            TestMapsterAdapter<Customer, CustomerDTO>(item, iterations, ref sMP);
 
-            TestExpressMapper<Customer, CustomerDTO>(item, iterations);
+            TypeAdapterConfig.GlobalSettings.Compiler = exp => exp.CompileFast();    //switch compiler
+            TypeAdapterConfig.GlobalSettings.Compile(typeof(Customer), typeof(CustomerDTO));    //recompile
+            item.Adapt<Customer, CustomerDTO>();    //exercise
+            TestMapsterAdapter<Customer, CustomerDTO>(item, iterations, ref sMF);
 
-            TestAutoMapper<Customer, CustomerDTO>(item, iterations);
+            ExpressMapper.Mapper.Map<Customer, CustomerDTO>(item);  //exercise
+            TestExpressMapper<Customer, CustomerDTO>(item, iterations, ref sEM);
+
+            Mapper.Map<Customer, CustomerDTO>(item);    //exercise
+            TestAutoMapper<Customer, CustomerDTO>(item, iterations, ref sAM);
         }
 
-        private static void TestSimple(Foo item, int iterations)
+        static void TestSimple(Foo item, int iterations)
         {
+
+
+
             Console.WriteLine();
 
             Console.WriteLine("Iterations : {0}", iterations);
 
-            TestMapsterAdapter<Foo, Foo>(item, iterations);
+            TypeAdapterConfig.GlobalSettings.Compiler = defaultCompiler;    //switch compiler
+            TypeAdapterConfig.GlobalSettings.Compile(typeof(Foo), typeof(Foo));    //recompile
+            item.Adapt<Foo, Foo>(); //exercise
+            TestMapsterAdapter<Foo, Foo>(item, iterations, ref sMP);
+
+            TypeAdapterConfig.GlobalSettings.Compiler = exp => exp.CompileFast();    //switch compiler
+            TypeAdapterConfig.GlobalSettings.Compile(typeof(Foo), typeof(Foo));    //recompile
+            item.Adapt<Foo, Foo>(); //exercise
+            TestMapsterAdapter<Foo, Foo>(item, iterations, ref sMF);
 
             //TestValueInjecter<Foo, Foo>(item, iterations);
 
-            TestExpressMapper<Foo, Foo>(item, iterations);
+            ExpressMapper.Mapper.Map<Foo, Foo>(item);   //exercise
+            TestExpressMapper<Foo, Foo>(item, iterations, ref sEM);
 
-            TestAutoMapper<Foo, Foo>(item, iterations);
+            Mapper.Map<Foo, Foo>(item);     //exercise
+            TestAutoMapper<Foo, Foo>(item, iterations, ref sAM);
         }
 
 
-        private static void TestCustomerNative(Customer item, int iterations)
+        static void TestCustomerNative(Customer item, int iterations)
         {
             Console.WriteLine("Handwritten Mapper:\t" + Loop<Customer>(item, get =>
             {
@@ -158,58 +168,57 @@ namespace Benchmark
                 dto.Name = get.Name;
                 dto.AddressCity = get.Address.City;
 
-                dto.Address = new Address() {Id = get.Address.Id, Street = get.Address.Street, Country = get.Address.Country, City = get.Address.City};
+                dto.Address = new Address() { Id = get.Address.Id, Street = get.Address.Street, Country = get.Address.Country, City = get.Address.City };
 
-                dto.HomeAddress = new AddressDTO() {Id = get.HomeAddress.Id, Country = get.HomeAddress.Country, City = get.HomeAddress.City};
+                dto.HomeAddress = new AddressDTO() { Id = get.HomeAddress.Id, Country = get.HomeAddress.Country, City = get.HomeAddress.City };
 
                 dto.Addresses = new AddressDTO[get.Addresses.Length];
                 for (int i = 0; i < get.Addresses.Length; i++)
                 {
-                    dto.Addresses[i] = new AddressDTO() {Id = get.Addresses[i].Id, Country = get.Addresses[i].Country, City = get.Addresses[i].City};
+                    dto.Addresses[i] = new AddressDTO() { Id = get.Addresses[i].Id, Country = get.Addresses[i].Country, City = get.Addresses[i].City };
                 }
 
                 dto.WorkAddresses = new List<AddressDTO>();
                 foreach (var workAddress in get.WorkAddresses)
                 {
-                    dto.WorkAddresses.Add(new AddressDTO() {Id = workAddress.Id, Country = workAddress.Country, City = workAddress.City});
+                    dto.WorkAddresses.Add(new AddressDTO() { Id = workAddress.Id, Country = workAddress.Country, City = workAddress.City });
                 }
 
             }, iterations));
         }
 
 
-        private static void TestMapsterAdapter<TSrc, TDest>(TSrc item, int iterations)
+        static void TestMapsterAdapter<TSrc, TDest>(TSrc item, int iterations, ref double counter)
             where TSrc : class
             where TDest : class, new()
         {
-            MapsterTime = Loop(item, get => TypeAdapter.Adapt<TSrc, TDest>(get), iterations);
-            Console.WriteLine("Mapster:\t\t" + MapsterTime);
-            TotalMapsterTime += MapsterTime;
+            var time = Loop(item, get => get.Adapt<TSrc, TDest>(), iterations);
+            Console.WriteLine("Mapster:\t\t" + time);
+            counter += time;
         }
 
-
-        private static void TestExpressMapper<TSrc, TDest>(TSrc item, int iterations)
+        static void TestExpressMapper<TSrc, TDest>(TSrc item, int iterations, ref double counter)
             where TSrc : class
             where TDest : class, new()
         {
-            ExpressMapperTime = Loop(item, get => ExpressMapper.Mapper.Map<TSrc, TDest>(get), iterations);
-            Console.WriteLine("ExpressMapper:\t\t" + ExpressMapperTime);
-            TotalExpressMapperTime += ExpressMapperTime;
+            var time = Loop(item, get => ExpressMapper.Mapper.Map<TSrc, TDest>(get), iterations);
+            Console.WriteLine("ExpressMapper:\t\t" + time);
+            counter += time;
         }
 
-        private static void TestAutoMapper<TSrc, TDest>(TSrc item, int iterations)
+        static void TestAutoMapper<TSrc, TDest>(TSrc item, int iterations, ref double counter)
             where TSrc : class
             where TDest : class, new()
         {
             //if (iterations > 50000)
             //    Console.WriteLine("AutoMapper still working please wait...");
 
-            AutomapperTime = Loop(item, get => Mapper.Map<TSrc, TDest>(get), iterations);
-            Console.WriteLine("AutoMapper:\t\t" + AutomapperTime);
-            TotalAutomapperTime += AutomapperTime;
+            var time = Loop(item, get => Mapper.Map<TSrc, TDest>(get), iterations);
+            Console.WriteLine("AutoMapper:\t\t" + time);
+            counter += time;
         }
 
-        private static long Loop<T>(T item, Action<T> action, int iterations = 1000000)
+        static long Loop<T>(T item, Action<T> action, int iterations = 1000000)
         {
             return Time(item, a =>
             {
@@ -220,7 +229,7 @@ namespace Benchmark
             });
         }
 
-        private static long Time<T>(T item, Action<T> action)
+        static long Time<T>(T item, Action<T> action)
         {
             var sw = new Stopwatch();
             sw.Start();
@@ -232,12 +241,12 @@ namespace Benchmark
 
         #region Data
 
-        private static Customer GetCustomer()
+        static Customer GetCustomer()
         {
             Customer c = new Customer()
             {
-                Address = new Address() {City = "istanbul", Country = "turkey", Id = 1, Street = "istiklal cad."},
-                HomeAddress = new Address() {City = "istanbul", Country = "turkey", Id = 2, Street = "istiklal cad."},
+                Address = new Address() { City = "istanbul", Country = "turkey", Id = 1, Street = "istiklal cad." },
+                HomeAddress = new Address() { City = "istanbul", Country = "turkey", Id = 2, Street = "istiklal cad." },
                 Id = 1,
                 Name = "Eduardo Najera",
                 Credit = 234.7m,
@@ -256,7 +265,7 @@ namespace Benchmark
             return c;
         }
 
-        private static Foo GetFoo()
+        static Foo GetFoo()
         {
             var o = new Foo
             {
@@ -266,7 +275,7 @@ namespace Benchmark
                 NullInt = 16,
                 DateTime = DateTime.Now,
                 Doublen = 2312112,
-                Foo1 = new Foo {Name = "foo one"},
+                Foo1 = new Foo { Name = "foo one" },
                 Foos = new List<Foo>
                 {
                     new Foo {Name = "j1", Int64 = 123, NullInt = 321},
@@ -279,8 +288,8 @@ namespace Benchmark
                     new Foo {Name = "a2"},
                     new Foo {Name = "a3"},
                 },
-                IntArr = new[] {1, 2, 3, 4, 5},
-                Ints = new[] {7, 8, 9},
+                IntArr = new[] { 1, 2, 3, 4, 5 },
+                Ints = new[] { 7, 8, 9 },
             };
 
             return o;
