6f88aa60f3d683a3830921f83b5b0943fe37d339
Chaowlert Chaisrchalermpol <chaowlert@gmail.com>
2020-01-19 10:09:17 +0700

    add label to Benchmark

--- src/Benchmark/Benchmarks/TestAll.cs
+++ src/Benchmark/Benchmarks/TestAll.cs
@@ -11,28 +11,28 @@ namespace Benchmark.Benchmarks
         [Params(1000, 10_000, 100_000, 1_000_000)]
         public int Iterations { get; set; }
 
-        [Benchmark]
+        [Benchmark(Description = "Mapster 4.1.1")]
         public void MapsterTest()
         {
             TestAdaptHelper.TestMapsterAdapter<Foo, Foo>(fooInstance, Iterations);
             TestAdaptHelper.TestMapsterAdapter<Customer, CustomerDTO>(customerInstance, Iterations);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "Mapster 4.1.1 (Codegen)")]
         public void CodegenTest()
         {
             TestAdaptHelper.TestCodeGen(fooInstance, Iterations);
             TestAdaptHelper.TestCodeGen(customerInstance, Iterations);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "ExpressMapper 1.9.1")]
         public void ExpressMapperTest()
         {
             TestAdaptHelper.TestExpressMapper<Foo, Foo>(fooInstance, Iterations);
             TestAdaptHelper.TestExpressMapper<Customer, CustomerDTO>(customerInstance, Iterations);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "AutoMapper 9.0.0")]
         public void AutoMapperTest()
         {
             TestAdaptHelper.TestAutoMapper<Foo, Foo>(fooInstance, Iterations);
