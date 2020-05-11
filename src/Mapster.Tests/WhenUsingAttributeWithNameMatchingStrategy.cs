b6d69d1c691f3351b1464da7e0cd2ce9b7269df8
Paulo Giron <paulo.giron@abreu.pt>
2020-05-11 17:41:29 +0100

    Fix property name match when using AdaptMember Attribute

---
+++ src/Mapster.Tests/WhenUsingAttributeWithNameMatchingStrategy.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapster.Tests
+{
+    [TestClass]
+    public class WhenUsingAttributeWithNameMatchingStrategy
+    {
+        [TestMethod]
+        public void Using_Attributes_With_NameMatchingStrategy()
+        {
+            TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.IgnoreCase);
+
+            var id = Guid.NewGuid();
+            var poco = new SimplePoco(id) { Name = "test" };
+            var dto = poco.Adapt<SimpleDto>();
+            dto.IdCode.ShouldBe(id);
+            dto.Name.ShouldBeNull();
+        }
+
+        public class SimplePoco
+        {
+            public SimplePoco(Guid id) { this.id = id; }
+
+            [AdaptMember("IdCode")]
+            private Guid id { get; }
+
+            [AdaptIgnore]
+            public string Name { get; set; }
+        }
+
+        public class SimpleDto
+        {
+            public Guid IdCode { get; set; }
+            public string Name { get; set; }
+        }
+
+
+    }
+}
