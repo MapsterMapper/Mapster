8248cc9843f77e9660f3512d70ed2927f25e5886
Chaowlert <chaowlert@th.ibm.com>
2020-08-02 01:44:36 +0700

    update tool

--- src/Mapster.Tool/Program.cs
+++ src/Mapster.Tool/Program.cs
@@ -32,70 +32,76 @@ namespace Mapster.Tool
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(opt =>
                 {
-                    using var dynamicContext = new AssemblyResolver(Path.GetFullPath(opt.Assembly));
-                    var assembly = dynamicContext.Assembly;
-                    var config = TypeAdapterConfig.GlobalSettings;
-                    config.SelfContainedCodeGeneration = true;
-                    config.Scan(assembly);
-
-                    foreach (var type in assembly.GetTypes())
+                    using (var dynamicContext = new AssemblyResolver(Path.GetFullPath(opt.Assembly)))
                     {
-                        if (!type.IsInterface)
-                            continue;
-                        var attr = type.GetCustomAttribute<MapperAttribute>();
-                        if (attr == null)
-                            continue;
-                        
-                        Console.WriteLine($"Processing: {type.FullName}");
-                        
-                        var definitions = new TypeDefinitions
-                        {
-                            Implements = new[] {type},
-                            Namespace = type.Namespace,
-                            TypeName = attr.Name ?? GetName(type.Name)
-                        };
-                        var translator = new ExpressionTranslator(definitions);
-                        var interfaces = GetAllInterfaces(type);
-                        foreach (var @interface in interfaces)
-                        {
-                            foreach (var prop in @interface.GetProperties())
-                            {
-                                if (!prop.PropertyType.IsGenericType)
-                                    continue;
-                                if (prop.PropertyType.GetGenericTypeDefinition() != typeof(Expression<>))
-                                    continue;
-                                var propArgs = prop.PropertyType.GetGenericArguments()[0];
-                                if (!propArgs.IsGenericType)
-                                    continue;
-                                if (propArgs.GetGenericTypeDefinition() != typeof(Func<,>))
-                                    continue;
-                                var funcArgs = propArgs.GetGenericArguments();
-                                var tuple = new TypeTuple(funcArgs[0], funcArgs[1]);
-                                var expr = config.CreateMapExpression(tuple, MapType.Projection);
-                                translator.VisitLambda(expr, ExpressionTranslator.LambdaType.PublicLambda, prop.Name);
-                            }
-                        }
-                        foreach (var @interface in interfaces)
-                        {
-                            foreach (var method in @interface.GetMethods())
-                            {
-                                if (method.IsGenericMethod)
-                                    continue;
-                                if (method.ReturnType == typeof(void))
-                                    continue;
-                                var methodArgs = method.GetParameters();
-                                if (methodArgs.Length < 1 || methodArgs.Length > 2)
-                                    continue;
-                                var tuple = new TypeTuple(methodArgs[0].ParameterType, method.ReturnType);
-                                var expr = config.CreateMapExpression(tuple, methodArgs.Length == 1 ? MapType.Map : MapType.MapToTarget);
-                                translator.VisitLambda(expr, ExpressionTranslator.LambdaType.PublicMethod, method.Name);
-                            }
-                        }
+                        var assembly = dynamicContext.Assembly;
+                        var config = TypeAdapterConfig.GlobalSettings;
+                        config.SelfContainedCodeGeneration = true;
+                        config.Scan(assembly);
 
-                        var code = translator.ToString();
-                        var path = Path.Combine(Path.GetFullPath(opt.Output), definitions.TypeName + ".cs");
-                        Directory.CreateDirectory(Path.GetDirectoryName(path));
-                        File.WriteAllText(path, code);
+                        foreach (var type in assembly.GetTypes())
+                        {
+                            if (!type.IsInterface)
+                                continue;
+                            var attr = type.GetCustomAttribute<MapperAttribute>();
+                            if (attr == null)
+                                continue;
+
+                            Console.WriteLine($"Processing: {type.FullName}");
+
+                            var definitions = new TypeDefinitions
+                            {
+                                Implements = new[] {type},
+                                Namespace = type.Namespace,
+                                TypeName = attr.Name ?? GetName(type.Name)
+                            };
+                            var translator = new ExpressionTranslator(definitions);
+                            var interfaces = GetAllInterfaces(type);
+                            foreach (var @interface in interfaces)
+                            {
+                                foreach (var prop in @interface.GetProperties())
+                                {
+                                    if (!prop.PropertyType.IsGenericType)
+                                        continue;
+                                    if (prop.PropertyType.GetGenericTypeDefinition() != typeof(Expression<>))
+                                        continue;
+                                    var propArgs = prop.PropertyType.GetGenericArguments()[0];
+                                    if (!propArgs.IsGenericType)
+                                        continue;
+                                    if (propArgs.GetGenericTypeDefinition() != typeof(Func<,>))
+                                        continue;
+                                    var funcArgs = propArgs.GetGenericArguments();
+                                    var tuple = new TypeTuple(funcArgs[0], funcArgs[1]);
+                                    var expr = config.CreateMapExpression(tuple, MapType.Projection);
+                                    translator.VisitLambda(expr, ExpressionTranslator.LambdaType.PublicLambda,
+                                        prop.Name);
+                                }
+                            }
+
+                            foreach (var @interface in interfaces)
+                            {
+                                foreach (var method in @interface.GetMethods())
+                                {
+                                    if (method.IsGenericMethod)
+                                        continue;
+                                    if (method.ReturnType == typeof(void))
+                                        continue;
+                                    var methodArgs = method.GetParameters();
+                                    if (methodArgs.Length < 1 || methodArgs.Length > 2)
+                                        continue;
+                                    var tuple = new TypeTuple(methodArgs[0].ParameterType, method.ReturnType);
+                                    var expr = config.CreateMapExpression(tuple,
+                                        methodArgs.Length == 1 ? MapType.Map : MapType.MapToTarget);
+                                    translator.VisitLambda(expr, ExpressionTranslator.LambdaType.PublicMethod,
+                                        method.Name);
+                                }
+                            }
+
+                            var code = translator.ToString();
+                            var path = Path.Combine(Path.GetFullPath(opt.Output), definitions.TypeName + ".g.cs");
+                            Directory.CreateDirectory(Path.GetDirectoryName(path));
+                            File.WriteAllText(path, code);
+                        }
                     }
                 });
         }
