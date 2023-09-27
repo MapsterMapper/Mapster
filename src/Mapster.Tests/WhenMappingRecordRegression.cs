1ab9810613230ed303d32e9ea84f9d50adf99fe2
DocSvartz <svartzdoctor@gmail.com>
2023-09-27 22:39:51 +0500

    remove whitespace and misprint _sourceEmailUpdate

--- src/Mapster.Tests/WhenMappingRecordRegression.cs
+++ src/Mapster.Tests/WhenMappingRecordRegression.cs
@@ -9,22 +9,16 @@ namespace Mapster.Tests
     /// Tests for https://github.com/MapsterMapper/Mapster/issues/537
     /// </summary>
     [TestClass]
-
     public class WhenMappingRecordRegression
     {
         [TestMethod]
         public void AdaptRecordToRecord()
         {
             var _source = new TestRecord() { X = 700 };
-
             var _destination = new TestRecord() { X = 500 };
-
             var _result = _source.Adapt(_destination);
 
-
-
             _result.X.ShouldBe(700);
-
             object.ReferenceEquals(_result, _destination).ShouldBeFalse();
         }
 
@@ -32,55 +26,33 @@ namespace Mapster.Tests
         public void AdaptPositionalRecordToPositionalRecord()
         {
             var _sourcePositional = new TestRecordPositional(600);
-
             var _destinationPositional = new TestRecordPositional(900);
-
             var _positionalResult = _sourcePositional.Adapt(_destinationPositional);
 
-
-
             _positionalResult.X.ShouldBe(600);
-
             object.ReferenceEquals(_destinationPositional, _positionalResult).ShouldBeFalse();
-
         }
 
         [TestMethod]
         public void AdaptRecordStructToRecordStruct()
         {
             var _sourceStruct = new TestRecordStruct() { X = 1000 };
-
             var _destinationStruct = new TestRecordStruct() { X = 800 };
-
             var _structResult = _sourceStruct.Adapt(_destinationStruct);
 
-
-
             _structResult.X.ShouldBe(1000);
-
             object.ReferenceEquals(_destinationStruct, _structResult).ShouldBeFalse();
-
-
         }
-
 
         [TestMethod]
         public void AdaptRecordToClass()
         {
             var _sourсe = new TestRecordPositional(200);
-
             var _destination = new TestClassProtectedCtr(400);
-
             var _result = _sourсe.Adapt(_destination);
 
-
-
-
             _destination.ShouldBeOfType<TestClassProtectedCtr>();
-
             _destination.X.ShouldBe(200);
-
-
             object.ReferenceEquals(_destination, _result).ShouldBeTrue();
         }
 
@@ -88,19 +60,11 @@ namespace Mapster.Tests
         public void AdaptClassToRecord()
         {
             var _sourсe = new TestClassProtectedCtr(200);
-
             var _destination = new TestRecordPositional(400);
-
             var _result = _sourсe.Adapt(_destination);
 
-
-
-
             _destination.ShouldBeOfType<TestRecordPositional>();
-
             _result.X.ShouldBe(200);
-
-
             object.ReferenceEquals(_destination, _result).ShouldBeFalse();
         }
 
@@ -108,43 +72,25 @@ namespace Mapster.Tests
         public void AdaptClassToClassPublicCtrIsNotInstanse()
         {
             var _source = new TestClassPublicCtr(200);
-
             var _destination = new TestClassPublicCtr(400);
-
             var _result = _source.Adapt(_destination);
 
-
-
-
             _destination.ShouldBeOfType<TestClassPublicCtr>();
-
             _destination.X.ShouldBe(200);
-
-
             object.ReferenceEquals(_destination, _result).ShouldBeTrue();
         }
-
 
         [TestMethod]
         public void AdaptClassToClassProtectdCtrIsNotInstanse()
         {
             var _source = new TestClassPublicCtr(200);
-
             var _destination = new TestClassProtectedCtr(400);
-
             var _result = _source.Adapt(_destination);
 
-
-
-
             _destination.ShouldBeOfType<TestClassProtectedCtr>();
-
             _destination.X.ShouldBe(200);
-
-
             object.ReferenceEquals(_destination, _result).ShouldBeTrue();
         }
-
 
         /// <summary>
         /// https://github.com/MapsterMapper/Mapster/issues/615
@@ -165,7 +111,6 @@ namespace Mapster.Tests
             };
 
             var destination = source.Adapt<DestinationWithStruct>();
-
             destination.TestStruct.Property.ShouldBe("A");
         }
 
@@ -176,24 +121,14 @@ namespace Mapster.Tests
         public void AdaptClassToClassFromPrivatePropertyIsNotInstanse()
         {
             var _source = new TestClassPublicCtr(200);
-
             var _destination = new TestClassProtectedCtrPrivateProperty(400, "Me");
-
             var _result = _source.Adapt(_destination);
 
-
-
-
             _destination.ShouldBeOfType<TestClassProtectedCtrPrivateProperty>();
-
             _destination.X.ShouldBe(200);
-
             _destination.Name.ShouldBe("Me");
-
-
             object.ReferenceEquals(_destination, _result).ShouldBeTrue();
         }
-
 
         /// <summary>
         /// https://github.com/MapsterMapper/Mapster/issues/427
@@ -202,55 +137,40 @@ namespace Mapster.Tests
         public void UpdateNullable()
         {
             var _source = new UserAccount("123", "123@gmail.com", new DateTime(2023, 9, 24));
-
             var _update = new UpdateUser
             {
                 Id = "123",
-
             };
-
             var configDate = new TypeAdapterConfig();
+
             configDate.ForType<UpdateUser, UserAccount>()
                 .Map(dest => dest.Modified, src => new DateTime(2025, 9, 24))
                 .IgnoreNullValues(true);
 
-
             _update.Adapt(_source, configDate);
 
-
-
-            var _sourseEmailUpdate = new UserAccount("123", "123@gmail.com", new DateTime(2023, 9, 24));
-
+            var _sourceEmailUpdate = new UserAccount("123", "123@gmail.com", new DateTime(2023, 9, 24));
             var _updateEmail = new UpdateUser
             {
                 Email = "245@gmail.com",
             };
 
-
             var config = new TypeAdapterConfig();
             config.ForType<UpdateUser, UserAccount>()
-
                 .IgnoreNullValues(true);
 
-
-            var _resultEmail = _updateEmail.Adapt(_sourseEmailUpdate, config);
-
+            var _resultEmail = _updateEmail.Adapt(_sourceEmailUpdate, config);
 
             _source.Id.ShouldBe("123");
             _source.Created.ShouldBe(new DateTime(2023, 9, 24));
             _source.Modified.ShouldBe(new DateTime(2025, 9, 24));
             _source.Email.ShouldBe("123@gmail.com");
-
-
-            _sourseEmailUpdate.Id.ShouldBe("123");
-            _sourseEmailUpdate.Created.ShouldBe(new DateTime(2023, 9, 24));
-            _sourseEmailUpdate.Modified.ShouldBe(null);
-            _sourseEmailUpdate.Email.ShouldBe("245@gmail.com");
-
-
+            _sourceEmailUpdate.Id.ShouldBe("123");
+            _sourceEmailUpdate.Created.ShouldBe(new DateTime(2023, 9, 24));
+            _sourceEmailUpdate.Modified.ShouldBe(null);
+            _sourceEmailUpdate.Email.ShouldBe("245@gmail.com");
 
         }
-
 
         /// <summary>
         /// https://github.com/MapsterMapper/Mapster/issues/524
@@ -259,23 +179,18 @@ namespace Mapster.Tests
         public void TSousreIsObjectUpdateUseDynamicCast()
         {
             var source = new TestClassPublicCtr { X = 123 };
-
             var _result = SomemapWithDynamic(source);
-
+            
             _result.X.ShouldBe(123);
-
         }
 
         TestClassPublicCtr SomemapWithDynamic(object source)
         {
             var dest = new TestClassPublicCtr { X = 321 };
             var dest1 = source.Adapt(dest,source.GetType(),dest.GetType());
-
+            
             return dest;
         }
-
-
-
 
 
         #region NowNotWorking
@@ -285,19 +200,11 @@ namespace Mapster.Tests
         public void DetectFakeRecord()
         {
             var _source = new TestClassPublicCtr(200);
-
             var _destination = new FakeRecord { X = 300 };
-
             var _result = _source.Adapt(_destination);
-
-
             _destination.X.ShouldBe(200);
-
             object.ReferenceEquals(_destination, _result).ShouldBeTrue();
-
-
         }
-
 
         /// <summary>
         /// https://github.com/MapsterMapper/Mapster/issues/430
@@ -307,20 +214,14 @@ namespace Mapster.Tests
         public void CollectionUpdate()
         {
             List<TestClassPublicCtr> sources = new()
-           {
+            {
                 new(541),
                 new(234)
-
             };
-
-
             var destination = new List<TestClassPublicCtr>();
-
-
             var _result = sources.Adapt(destination);
 
             destination.Count.ShouldBe(_result.Count);
-
         }
 
         /// <summary>
@@ -333,11 +234,9 @@ namespace Mapster.Tests
         public void TSousreIsObjectUpdate()
         {
             var source = new TestClassPublicCtr { X = 123 };
-
             var _result = Somemap(source);
 
             _result.X.ShouldBe(123);
-
         }
 
         TestClassPublicCtr Somemap(object source)
@@ -350,7 +249,6 @@ namespace Mapster.Tests
 
         #endregion NowNotWorking
 
-
     }
 
 
@@ -358,19 +256,11 @@ namespace Mapster.Tests
 
     public class FakeRecord
     {
-        protected FakeRecord(FakeRecord fake)
-        {
-
-        }
-
-        public FakeRecord()
-        {
-
-        }
+        protected FakeRecord(FakeRecord fake) { }
+        public FakeRecord() { }
 
         public int X { get; set; }
     }
-
 
     class UserAccount
     {
@@ -379,9 +269,7 @@ namespace Mapster.Tests
             Id = id;
             Email = email;
             Created = created;
-
         }
-
         protected UserAccount() { }
 
         public string Id { get; set; }
@@ -394,12 +282,9 @@ namespace Mapster.Tests
     {
         public string? Id { get; set; }
         public string? Email { get; set; }
-
         public DateTime? Created { get; set; }
         public DateTime? Modified { get; set; }
-
     }
-
 
     class DestinationWithStruct
     {
@@ -419,20 +304,15 @@ namespace Mapster.Tests
     struct TestStruct
     {
         public string Property { get; }
-
         public TestStruct(string property) : this()
         {
             Property = property;
         }
     }
 
-
     class TestClassPublicCtr
     {
-        public TestClassPublicCtr()
-        {
-
-        }
+        public TestClassPublicCtr() { }
 
         public TestClassPublicCtr(int x)
         {
@@ -442,13 +322,9 @@ namespace Mapster.Tests
         public int X { get; set; }
     }
 
-
     class TestClassProtectedCtr
     {
-        protected TestClassProtectedCtr()
-        {
-
-        }
+        protected TestClassProtectedCtr() { }
 
         public TestClassProtectedCtr(int x)
         {
@@ -458,13 +334,9 @@ namespace Mapster.Tests
         public int X { get; set; }
     }
 
-
     class TestClassProtectedCtrPrivateProperty
     {
-        protected TestClassProtectedCtrPrivateProperty()
-        {
-
-        }
+        protected TestClassProtectedCtrPrivateProperty() { }
 
         public TestClassProtectedCtrPrivateProperty(int x, string name)
         {
@@ -477,11 +349,8 @@ namespace Mapster.Tests
         public string Name { get; private set; }
     }
 
-
     record TestRecord()
     {
-
-
         public int X { set; get; }
     }
 
