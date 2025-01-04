4deea1a08701d2741ecbf0f366196204dc33e2c9
DocSvartz <svartzdoctor@gmail.com>
2025-01-04 20:15:20 +0500

    add mapping init PropertyTest

---
+++ src/Mapster.Tests/WhenMappingInitProperty.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Mapster.Tests;
+
+[TestClass]
+public class WhenMappingInitProperty
+{
+
+    #region Tests
+    /// <summary>
+    /// From Issue #672 
+    /// https://github.com/MapsterMapper/Mapster/issues/672
+    /// </summary>
+    [TestMethod]
+    public void WhenMappingToHiddenandNewInitFieldDestination()
+    {
+        var source = new Source672() { Id = 156};
+        var c =  source.Adapt<BDestination>(); 
+        var s = source.Adapt(new BDestination());
+
+        ((ADestination)c).Id.ShouldBe(156);
+        s.Id.ShouldBe(156);
+    }
+
+    #endregion Tests
+
+
+    #region TestClasses
+
+    class Source672
+    {
+        public long Id { get; init; }
+    }
+
+    class ADestination
+    {
+        public int Id { get; init; }
+    }
+
+    class BDestination : ADestination
+    {
+        public new long Id { get; init; }
+    }
+
+
+    #endregion TestClasses
+}
