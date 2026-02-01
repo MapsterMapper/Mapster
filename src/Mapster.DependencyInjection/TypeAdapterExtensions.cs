1336b2c897adfdae2839e0d7472f09b9087e1e02
DevTKSS <info@technische-konstruktion.com>
2026-02-01 23:41:23 +0100

    chore: Update to file scoped namespaces and apply pragma warning where needed for IDE0130

--- src/Mapster.DependencyInjection/TypeAdapterExtensions.cs
+++ src/Mapster.DependencyInjection/TypeAdapterExtensions.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using MapsterMapper;
+﻿namespace Mapster;
 
-namespace Mapster
+public static class TypeAdapterExtensions
 {
-    public static class TypeAdapterExtensions
+    internal static U GetValueOrDefault<T, U>(this IDictionary<T, U> dict, T key)
     {
-        internal static U GetValueOrDefault<T, U>(this IDictionary<T, U> dict, T key)
-        {
-            return dict.TryGetValue(key, out var value) ? value : default;
-        }
+        return dict.TryGetValue(key, out var value) ? value : default;
+    }
 
-        public static TService GetService<TService>(this MapContext context)
-        {
-            var sp = (IServiceProvider) context?.Parameters.GetValueOrDefault(ServiceMapper.DI_KEY);
-            if (sp == null)
-                throw new InvalidOperationException("Mapping must be called using ServiceAdapter");
-            return (TService)sp.GetService(typeof(TService));
-        }
+    public static TService GetService<TService>(this MapContext context)
+    {
+        var sp = (IServiceProvider) context?.Parameters.GetValueOrDefault(ServiceMapper.DI_KEY);
+        if (sp == null)
+            throw new InvalidOperationException("Mapping must be called using ServiceAdapter");
+        return (TService)sp.GetService(typeof(TService));
     }
 }
