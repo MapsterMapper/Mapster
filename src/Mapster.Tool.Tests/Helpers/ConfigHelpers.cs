77b53add260fd32e4dbb6c6d67d92ecdbe414452
DocSvartz <svartzdoctor@gmail.com>
2026-09-07 09:30:07 +0500

    feat(test): Improvements in Mapster.Tool.Tests : - sync TFM with Mapster.Tool - added ConfigHelpers for testing

---
+++ src/Mapster.Tool.Tests/Helpers/ConfigHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Mapster.Tool.Tests.Helpers
+{
+    internal static class ConfigHelpers
+    {
+        internal static MapperOptions optMappers => new MapperOptions() { Assembly = Assembly.GetExecutingAssembly().Location, Output = Path.GetTempPath() };
+        internal static ModelOptions optModels = new ModelOptions() { Assembly = Assembly.GetExecutingAssembly().Location, Output = Path.GetTempPath() };
+        internal static ExtensionOptions optExtentions = new ExtensionOptions() { Assembly = Assembly.GetExecutingAssembly().Location, Output = Path.GetTempPath() };
+    }
+}
